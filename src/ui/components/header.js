4f1c3a9f287f1675b9c89ecec23f8972b9665b65
Hidzero <ra202010790@univassouras.edu.br>
2024-09-06 16:42:18 -0300

    Agora foi

--- src/ui/components/header.js
+++ src/ui/components/header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark torresmoColor">
         <div class="container-fluid">
+            <img src="./logo-porco.png" alt="Logo do restaurante" width="150" height="80" />
             <img src="./logo.png" alt="Logo do restaurante" width="80" height="80" />
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
